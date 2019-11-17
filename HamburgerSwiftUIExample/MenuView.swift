327cffaf76d708084fc4d8ed5193d419ad06d93f
Станислав Шияновский <st-small@rambler.ru>
2019-11-17 17:25:44 +0200

    Finished project.

---
+++ HamburgerSwiftUIExample/MenuView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuView.swift
+//  HamburgerSwiftUIExample
+//
+//  Created by Станислав Шияновский on 11/17/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import SwiftUI
+
+public struct MenuView: View {
+    public var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+                .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
+        }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
